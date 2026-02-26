63de932bf0fa543ad8111d0445089499d76d3d53
Joel Ayala <computacioneizi85@gmail.com>
2026-02-26 11:47:14 -0600

    fix login 2

--- app.py
+++ app.py
@@ -2,37 +2,41 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# ------------------------
-# LOGIN
-# ------------------------
-
+# -------------------------
+# MOSTRAR LOGIN
+# -------------------------
 @app.route("/")
-def login():
+@app.route("/login", methods=["GET"])
+def login_page():
     return render_template("login.html")
 
+
+# -------------------------
+# PROCESAR LOGIN
+# -------------------------
 @app.route("/login", methods=["POST"])
-def do_login():
+def login():
+
     usuario = request.form.get("usuario")
     password = request.form.get("password")
 
-    # acceso direccion
     if usuario == "direccion" and password == "1234":
         return redirect(url_for("admin"))
 
-    return redirect(url_for("login"))
+    return render_template("login.html", error="Usuario o contraseña incorrectos")
 
-# ------------------------
+
+# -------------------------
 # PANEL ADMIN
-# ------------------------
-
+# -------------------------
 @app.route("/admin")
 def admin():
     return render_template("admin.html")
 
-# ------------------------
-# ALUMNOS
-# ------------------------
 
+# -------------------------
+# ALUMNOS
+# -------------------------
 @app.route("/nuevo_alumno")
 def nuevo_alumno():
     return render_template("nuevo_alumno.html")
@@ -41,10 +45,10 @@ def nuevo_alumno():
 def alumno():
     return render_template("alumno.html")
 
-# ------------------------
-# MAESTROS
-# ------------------------
 
+# -------------------------
+# MAESTROS
+# -------------------------
 @app.route("/nuevo_maestro")
 def nuevo_maestro():
     return render_template("nuevo_maestro.html")
@@ -53,7 +57,6 @@ def nuevo_maestro():
 def maestro():
     return render_template("maestro.html")
 
-# ------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
