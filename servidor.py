eb4feb78b7bedf3c0957de936328bbfe4eba3ca9
Joel Ayala <computacioneizi85@gmail.com>
2026-02-26 10:51:11 -0600

    fix render structure

--- servidor.py
+++ servidor.py
@@ -1,4 +1,7 @@
-from app import app
+from flask import Flask, render_template
 
-if **name** == "**main**":
-app.run()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
