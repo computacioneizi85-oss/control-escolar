2036fdba3538fab7c46d7d6814d0e8b0de96c7ab
Joel Ayala <computacioneizi85@gmail.com>
2026-03-06 10:13:36 -0600

    fix blueprint auth

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,18 +1,26 @@
+from flask import Blueprint, render_template, request, redirect, session
+from database.mongo import usuarios
+
+# Crear blueprint
+auth_bp = Blueprint("auth", __name__)
+
+# Página de login
+@auth_bp.route("/")
+def login_page():
+    return render_template("login.html")
+
+
+# Procesar login
 @auth_bp.route("/login", methods=["POST"])
 def login():
 
     usuario = request.form.get("usuario")
     password = request.form.get("password")
 
-    print("USUARIO RECIBIDO:", usuario)
-    print("PASSWORD RECIBIDO:", password)
-
     user = usuarios.find_one({
         "usuario": usuario,
         "password": password
     })
-
-    print("USUARIO ENCONTRADO EN MONGO:", user)
 
     if user:
 
@@ -25,4 +33,11 @@ def login():
         if user["rol"] == "maestro":
             return redirect("/maestro")
 
+    return redirect("/")
+
+
+# Logout
+@auth_bp.route("/logout")
+def logout():
+    session.clear()
     return redirect("/")
