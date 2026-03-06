57fb31417e0418ab73e5681b05b67d1577f8ed24
Joel Ayala <computacioneizi85@gmail.com>
2026-03-06 10:25:46 -0600

    dashboard funcional

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, session, redirect
+from flask import Blueprint, render_template, request, redirect, session
+from database.mongo import alumnos, grupos
 
 admin_bp = Blueprint("admin", __name__)
-
 
 @admin_bp.route("/admin")
 def admin_panel():
@@ -12,4 +12,45 @@ def admin_panel():
     if session["rol"] != "admin":
         return redirect("/")
 
-    return render_template("admin.html")
+    lista_alumnos = list(alumnos.find())
+    lista_grupos = list(grupos.find())
+
+    return render_template(
+        "admin.html",
+        alumnos=lista_alumnos,
+        grupos=lista_grupos
+    )
+
+
+# =========================
+# Crear grupo
+# =========================
+
+@admin_bp.route("/crear_grupo", methods=["POST"])
+def crear_grupo():
+
+    nombre = request.form.get("grupo")
+
+    grupos.insert_one({
+        "nombre": nombre
+    })
+
+    return redirect("/admin")
+
+
+# =========================
+# Crear alumno
+# =========================
+
+@admin_bp.route("/crear_alumno", methods=["POST"])
+def crear_alumno():
+
+    nombre = request.form.get("nombre")
+    grupo = request.form.get("grupo")
+
+    alumnos.insert_one({
+        "nombre": nombre,
+        "grupo": grupo
+    })
+
+    return redirect("/admin")
