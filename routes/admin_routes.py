5af7fd2064246464c5de4f88f866e81a70ca8442
Joel Ayala <computacioneizi85@gmail.com>
2026-03-06 11:10:46 -0600

    dashboard funcional 4

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -54,3 +54,85 @@ def crear_alumno():
     })
 
     return redirect("/admin")
+
+@admin_bp.route("/crear_alumno", methods=["POST"])
+def crear_alumno():
+
+    nombre = request.form.get("nombre")
+    grupo = request.form.get("grupo")
+
+    alumnos.insert_one({
+
+        "nombre": nombre,
+        "grupo": grupo
+
+    })
+
+    return redirect("/alumnos")
+
+@admin_bp.route("/alumnos")
+def ver_alumnos():
+
+    if "rol" not in session:
+        return redirect("/")
+
+    lista_alumnos = list(alumnos.find())
+    lista_grupos = list(grupos.find())
+
+    return render_template(
+        "alumnos.html",
+        alumnos=lista_alumnos,
+        grupos=lista_grupos
+    )
+
+from bson.objectid import ObjectId
+
+
+@admin_bp.route("/eliminar_alumno/<id>")
+def eliminar_alumno(id):
+
+    alumnos.delete_one({
+
+        "_id": ObjectId(id)
+
+    })
+
+    return redirect("/alumnos")
+
+@admin_bp.route("/editar_alumno/<id>")
+def editar_alumno(id):
+
+    alumno = alumnos.find_one({
+
+        "_id": ObjectId(id)
+
+    })
+
+    lista_grupos = list(grupos.find())
+
+    return render_template(
+        "editar_alumno.html",
+        alumno=alumno,
+        grupos=lista_grupos
+    )
+
+@admin_bp.route("/actualizar_alumno/<id>", methods=["POST"])
+def actualizar_alumno(id):
+
+    nombre = request.form.get("nombre")
+    grupo = request.form.get("grupo")
+
+    alumnos.update_one(
+
+        {"_id": ObjectId(id)},
+
+        {"$set": {
+
+            "nombre": nombre,
+            "grupo": grupo
+
+        }}
+
+    )
+
+    return redirect("/alumnos")
