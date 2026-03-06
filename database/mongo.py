5d11710f96fd99de35ab2031bee1b9e7da77a2c5
Joel Ayala <computacioneizi85@gmail.com>
2026-03-06 09:45:07 -0600

    mongodb base 5

--- database/mongo.py
+++ database/mongo.py
@@ -1,13 +1,9 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://joel:CAMELLO2052@ethhsnm.mongodb.net/control_escolar?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://joel:CAMELLO2052@ethhsnm.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_URI)
 
 db = client["control_escolar"]
 
 usuarios = db["usuarios"]
-alumnos = db["alumnos"]
-grupos = db["grupos"]
-materias = db["materias"]
-reportes = db["reportes"]
