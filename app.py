fffe7da5d818c6936a548aaa18f9f66b36c97c2d
Joel Ayala <computacioneizi85@gmail.com>
2026-03-09 07:47:07 -0600

    mongodb env variable

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 from flask import Flask, render_template, session, redirect
-from config import MONGO_URI
-from database.mongo import init_db
 
 # BLUEPRINTS
 from routes.auth_routes import auth_bp
 from routes.admin_routes import admin_bp
 
-# CREAR APP
 app = Flask(__name__)
 
-app.secret_key = "control_escolar_secret_key"
+app.secret_key = "control_escolar_secret"
 
-# INICIAR BASE DE DATOS
-init_db(MONGO_URI)
 
-# REGISTRAR RUTAS
+# Registrar blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp)
 
-
-# -----------------------
-# PAGINA PRINCIPAL
-# -----------------------
 
 @app.route("/")
 def index():
@@ -37,21 +28,12 @@ def index():
     return render_template("login.html")
 
 
-# -----------------------
-# LOGOUT
-# -----------------------
-
 @app.route("/logout")
 def logout():
 
     session.clear()
-
     return redirect("/")
 
-
-# -----------------------
-# ARRANQUE LOCAL
-# -----------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database/mongo.py
+++ database/mongo.py
@@ -1,15 +1,8 @@
-import os
 from pymongo import MongoClient
+import os
 
-MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI")
 
 client = MongoClient(MONGO_URI)
 
 db = client["control_escolar"]
-
-usuarios = db["usuarios"]
-alumnos = db["alumnos"]
-grupos = db["grupos"]
-materias = db["materias"]
-calificaciones = db["calificaciones"]
-reportes = db["reportes"]
