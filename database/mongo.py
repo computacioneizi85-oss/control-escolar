4e5a9a432beb2f8fee2e1aa03117cc92aec75ac1
Joel Ayala <computacioneizi85@gmail.com>
2026-03-09 07:57:34 -0600

    fix colecciones mongo

--- database/mongo.py
+++ database/mongo.py
@@ -6,3 +6,13 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 
 db = client["control_escolar"]
+
+# COLECCIONES
+
+usuarios = db["usuarios"]
+alumnos = db["alumnos"]
+grupos = db["grupos"]
+maestros = db["maestros"]
+materias = db["materias"]
+reportes = db["reportes"]
+calificaciones = db["calificaciones"]
