bc684560cc94a4377c65774926f68a0898e67419
Joel Ayala <computacioneizi85@gmail.com>
2026-04-13 14:31:40 -0600

    actualizando 11

--- routes/alumno_routes.py
+++ routes/alumno_routes.py
@@ -5,7 +5,7 @@ alumno_bp = Blueprint("alumno", __name__)
 
 
 def verificar_alumno():
-    return "rol" in session and session["rol"] == "alumno"
+    return session.get("rol") == "alumno"
 
 
 @alumno_bp.route("/panel_alumno")
@@ -14,7 +14,11 @@ def panel_alumno():
     if not verificar_alumno():
         return redirect("/")
 
-    alumno = alumnos.find_one({"nombre": session["usuario"]})
+    # 🔥 CORREGIDO (usar usuario)
+    alumno = alumnos.find_one({"usuario": session["usuario"]})
+
+    if not alumno:
+        return "Alumno no encontrado"
 
     return render_template(
         "panel_alumno.html",
